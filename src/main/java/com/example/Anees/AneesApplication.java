4abfdc2f81d2137f99d67adbabe7627603ddc8c7
unknown <a.alsulami@wakeb.tech>
2023-02-28 11:05:52 +0300

    creatre Anees project with stander structur

---
+++ src/main/java/com/example/Anees/AneesApplication.java
@@ -0,0 +1,13 @@
+package com.example.Anees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AneesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AneesApplication.class, args);
+	}
+
+}
